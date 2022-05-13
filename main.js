70d86817175860c3c711cc5fe5367798ba0c6eb2
Kevin Johnson <krjohnson111@gmail.com>
2022-05-13 19:58:37 -0400

    Put main code in main function

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 import * as cube from './modules/cube.js';
 
-let c = cube.create();
-console.log(cube.to_string(c));
-
-let rotations = Object.keys(cube.rotation);
-
-for (const elem of rotations) {
-    cube.rotate(c, cube.rotation[elem]);
-    console.log(elem);
+const main = () => {
+    let c = cube.create();
     console.log(cube.to_string(c));
+
+    let rotations = Object.keys(cube.rotation);
+
+    for (const elem of rotations) {
+        cube.rotate(c, cube.rotation[elem]);
+        console.log(elem);
+        console.log(cube.to_string(c));
+    }
+
+    rotations.reverse();
+
+    for (const elem of rotations) {
+        cube.rotate(c, -cube.rotation[elem]);
+        console.log('-' + elem);
+        console.log(cube.to_string(c));
+    }
 }
 
-rotations.reverse();
-
-for (const elem of rotations) {
-    cube.rotate(c, -cube.rotation[elem]);
-    console.log("-" + elem);
-    console.log(cube.to_string(c));
-}
+main()
