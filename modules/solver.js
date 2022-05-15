8e0ef2ffb233bfe504641b6cdf7d5998205138da
Kevin Johnson <krjohnson111@gmail.com>
2022-05-15 10:28:23 -0400

    Solver passes the allowed rotations to the impl function

--- modules/solver.js
+++ modules/solver.js
@@ -6,25 +6,6 @@ export const solve = (cube) => {
         return [];
     }
 
-    const solution = solve_impl(cube, solved_cube, null, null, null, 6);
-    if (solution !== null) {
-        solution.reverse();
-    }
-    return solution;
-};
-
-const solve_impl = (cube, solved_cube, last_rot, last_axis1, last_axis2, max_depth) => {
-    if (max_depth === 0) {
-        return null;
-    }
-
-    const normalize_rotation = (rotation) => {
-        if (rotation === null) {
-            return null;
-        }
-        return rubik.base_rotation(rotation);
-    };
-
     // Double rotations get tried first
     const allowed_rotations = [
         rubik.rotation.F * 2,
@@ -46,6 +27,27 @@ const solve_impl = (cube, solved_cube, last_rot, last_axis1, last_axis2, max_dep
         rubik.rotation.U,
         rubik.rotation.U * -1,
     ];
+
+    const solution = solve_impl(cube, solved_cube, allowed_rotations, null, null, null, 6);
+    if (solution !== null) {
+        solution.reverse();
+    }
+    return solution;
+};
+
+const solve_impl = (cube, solved_cube, allowed_rotations,
+                    last_rot, last_axis1, last_axis2, max_depth) => {
+    if (max_depth === 0) {
+        return null;
+    }
+
+    const normalize_rotation = (rotation) => {
+        if (rotation === null) {
+            return null;
+        }
+        return rubik.base_rotation(rotation);
+    };
+
     for (const rotation of allowed_rotations) {
         const base_rot = rubik.base_rotation(rotation);
         let axis;
@@ -66,18 +68,16 @@ const solve_impl = (cube, solved_cube, last_rot, last_axis1, last_axis2, max_dep
 
         // Can't do a move on the same side as the last move,
         // or a move on the same axis as the last 2 moves
-        // e.g. F followed by F', or F B followed by F
+        // e.g. F followed by F' (would cancel out) or F B followed by F (equivalent to F2 B)
         if (base_rot === last_rot || (axis === last_axis1 && axis === last_axis2)) {
             continue;
         }
-        // console.log('    '.repeat(max_depth) + rubik.rotations_to_string([rotation]));
         rubik.rotate(cube, rotation);
-        // console.log(cube);
         if (rubik.is_equal(cube, solved_cube)) {
             return [rotation];
         }
         const possible_solution = solve_impl(
-            cube, solved_cube, base_rot, last_axis2, axis, max_depth - 1);
+            cube, solved_cube, allowed_rotations, base_rot, last_axis2, axis, max_depth - 1);
         if (possible_solution !== null) {
             possible_solution.push(rotation);
             return possible_solution;
